82416e7915b6049f80acb786462e6902fd1a2f5f
josueJURE <josue.jure@gmail.com>
2020-06-06 05:42:21 +0100

    Add button and EventListener

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 
-const url = "https://api.wheretheiss.at/v1/satellites/25544"
-  async function getData() {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data.latitude);
-  }
-  getData();
+var btn = document.getElementById("btn")
+btn.addEventListener("click", function(){
+  const url = "https://api.wheretheiss.at/v1/satellites/25544"
+    async function getData() {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data.latitude);
+    }
+    getData();
+})
